e31d3f373851bbc68b874f3675237d8e336b0c26
Git-Shira <shirae447@gmail.com>
2025-02-05 14:10:21 +0200

    Update index.js
    Use environment variable for PORT instead of hardcoded value

--- BACK/index.js
+++ BACK/index.js
@@ -123,7 +123,7 @@ function parseInterval(interval) {
     return 60;
 }
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
